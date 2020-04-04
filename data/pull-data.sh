619b6ae571e391e7f868abc2561fbde5d324cb88
Sam <samfelt@users.noreply.github.com>
2020-04-03 18:31:48 -0700

    Update pull script

--- data/pull-data.sh
+++ data/pull-data.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/env bash
 
 FILENAME='WA-data.json'
+URL='https://covidtracking.com/api/states/daily?state=WA'
+
+curr_sha=$(shasum $FILENAME | cut -d " " -f1)
+new_sha=$(curl -s $URL | shasum | cut -d " " -f1)
+
+if [ "$curr_sha" == "$new_sha" ]; then
+    echo "No new changes"
+    exit
+fi
+
 mv "$FILENAME" "$FILENAME.bak"
-curl https://covidtracking.com/api/states/daily?state=WA > $FILENAME
+curl -s $URL > $FILENAME
