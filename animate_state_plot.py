34e51c1fca3568986156eab6c37a5ffd2b71715e
Sam <samfelt@users.noreply.github.com>
2020-05-08 13:46:57 -0700

    Animate up to 5 states at a time

--- animate_state_plot.py
+++ animate_state_plot.py
@@ -13,6 +13,7 @@ from StateData import StateData
 
 ALL_STATE_DATA='data/all-states-daily.json'
 COUNTRY_DATA='data/country-daily.json'
+colors = ['blue', 'red', 'green', 'yellow', 'purple']
 
 def banner():
     print('+========================================+')
@@ -35,50 +36,67 @@ def extract_single_state(data, state_name):
     return state_data
 
 def animate(i):
-    p = plt.plot(state.dates[:i], state.positive[:i])
-    p[0].set_color('green')
+    color_count = 0
+    plt.legend(names)
+    for state in states:
+        p = plt.plot(state.dates[:i], state.positive[:i])
+        p[0].set_color(colors[color_count])
+        color_count += 1
 
 banner()
 logger = set_up_logger()
 
-# Pick state that we should plot
+# Pick states that we should plot
+names = []
 if len(sys.argv)==1:
-    name = 'WA'
+    names.append('WA')
 else:
-    name = sys.argv[1]
+    for i in range(1,len(sys.argv)):
+        names.append(sys.argv[i])
 
 # Check if we want country data (special case)
-state = StateData(name)
-if name == "US":
-    logger.info(f"Extracting data for the US")
-    logger.info(f"Data file: {COUNTRY_DATA}")
-    with open(COUNTRY_DATA, 'r') as json_file:
-        state_data = json.load(json_file)
-else:
-    logger.info(f"Extracting data for {state.name}")
-    logger.info(f"Data file: {ALL_STATE_DATA}")
-    with open(ALL_STATE_DATA, 'r') as json_file:
-        data = json.load(json_file)
-    state_data = extract_single_state(data, state.name)
+states = []
+for name in names:
+    state = StateData(name)
+    if name == "US":
+        logger.info(f"Extracting data for the US")
+        logger.info(f"Data file: {COUNTRY_DATA}")
+        with open(COUNTRY_DATA, 'r') as json_file:
+            state_data = json.load(json_file)
+    else:
+        logger.info(f"Extracting data for {state.name}")
+        logger.info(f"Data file: {ALL_STATE_DATA}")
+        with open(ALL_STATE_DATA, 'r') as json_file:
+            data = json.load(json_file)
+        state_data = extract_single_state(data, state.name)
 
-logger.info("Importing data")
-state.import_json(state_data)
-logger.info(f"Start Date: {state.start_date}")
-logger.info(f"{state.number_of_data_points} days imported")
+    logger.info("Importing data")
+    state.import_json(state_data)
+    logger.info(f"Start Date: {state.start_date}")
+    logger.info(f"{state.number_of_data_points} days imported")
+    states.append(state)
 
-# Setup figure
-#fig, ax = plt.subplots()
-#xdata, ydata = [], [] 
-#line, = plt.plot([], [], 'ro')
+# Normalize the dates so the days are plotting at the same time
+earliest_state = min(states, key=lambda x:x.start_date)
+logger.info(f"Normalizing Timelines to start on: {earliest_state.start_date}")
+for s in states:
+    diff = s.start_date - earliest_state.start_date
+    additional_days = earliest_state.dates[:diff.days]
+    new_dates = np.concatenate((additional_days, s.dates))
+    s.dates = new_dates
+
+    leading_zeros = np.zeros((len(additional_days),), dtype=int)
+    new_data = np.concatenate((leading_zeros, s.positive))
+    s.positive = new_data
 
 fig = plt.figure()
 plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
 plt.subplots_adjust(bottom = 0.2, top = 0.9)
-plt.ylabel('Y-label')
-plt.xlabel('X-label')
-plt.title("Animating Graphs")
+plt.ylabel('Positive Cases')
+plt.xlabel('Date')
+plt.title("COVID Timeline")
 
-animator = ani.FuncAnimation(fig, animate, interval = 200)
+animator = ani.FuncAnimation(fig, animate, interval = 175)
 plt.show()
 
 logger.info("plot-data Complete")
